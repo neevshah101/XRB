eae1af46c7957e4b0c8206262ee76739cb017a4e
Neev Shah <neevshah2001@gmail.com>
2024-11-27 14:56:32 -0700

    renamed binary folder to binary_star and added binary,star files to gitignore

---
+++ binary_star/puma_submit.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# --------------------
+### Directives Section
+# --------------------
+#SBATCH --job-name=binary_array
+#SBATCH --account=binboom
+#SBATCH --partition=standard
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --time=72:00:00
+#SBATCH -o no_rot_sc_th_%A_%a.out
+#SBATCH --array=0-0
+# --------------------
+### Code Section
+# --------------------
+export OMP_NUM_THREADS=16
+
+# Array of folder names
+folders=(m1_60_m2_40_p_6_BD_no_rot_sc_th)
+
+# Navigate to the appropriate folder
+cd /groups/mrenzo/Neev/Projects/XRB/binary/${folders[$SLURM_ARRAY_TASK_ID]}
+./clean && ./mk
+./rn
