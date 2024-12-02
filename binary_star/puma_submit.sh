dad9e0a3b0dc7495123c05858d472bda921bba34
Neev Shah <neevshah2001@gmail.com>
2024-12-02 13:51:30 -0700

    changed Lnuc_He limits

--- binary_star/puma_submit.sh
+++ binary_star/puma_submit.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=16
 #SBATCH --time=72:00:00
-#SBATCH -o no_rot_sc_th_%A_%a.out
+#SBATCH -o no_rot_sc_th_basic_net%A_%a.out
 #SBATCH --array=0-0
 # --------------------
 ### Code Section
@@ -16,9 +16,9 @@
 export OMP_NUM_THREADS=16
 
 # Array of folder names
-folders=(m1_60_m2_40_p_6_BD_no_rot_sc_th)
+folders=(m1_60_m2_40_p_6_BD_no_rot_sc_th_basic_net)
 
 # Navigate to the appropriate folder
-cd /groups/mrenzo/Neev/Projects/XRB/binary/${folders[$SLURM_ARRAY_TASK_ID]}
+cd /groups/mrenzo/Neev/Projects/XRB/binary_star/${folders[$SLURM_ARRAY_TASK_ID]}
 ./clean && ./mk
-./rn
+./re 27000
