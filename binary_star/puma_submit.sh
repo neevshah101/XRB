ab71bf0b92ae0a857858eaf9acf5f5f4149bb68b
Neev Shah <neevshah2001@gmail.com>
2024-12-04 12:14:54 -0700

    running 70+50

--- binary_star/puma_submit.sh
+++ binary_star/puma_submit.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=16
 #SBATCH --time=72:00:00
-#SBATCH -o no_rot_sc_th_basic_net%A_%a.out
+#SBATCH -o no_rot_sc_th_basic_net_inc_mass%A_%a.out
 #SBATCH --array=0-0
 # --------------------
 ### Code Section
@@ -16,9 +16,9 @@
 export OMP_NUM_THREADS=16
 
 # Array of folder names
-folders=(m1_60_m2_40_p_6_BD_no_rot_sc_th_basic_net)
+folders=(m1_60_m2_40_p_6_BD_no_rot_sc_th_basic_net_inc_mass)
 
 # Navigate to the appropriate folder
 cd /groups/mrenzo/Neev/Projects/XRB/binary_star/${folders[$SLURM_ARRAY_TASK_ID]}
-./mk && ./rn
-./re 28000
+./clean && ./mk
+./rn
