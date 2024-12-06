e8147eafd3c21a5bbf17873abcba89afdfbf9186
Neev Shah <neevshah2001@gmail.com>
2024-12-05 23:46:40 -0700

    re running low z with superad, and 60+40 at Z=0.0146

--- binary_star/puma_submit.sh
+++ binary_star/puma_submit.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=16
 #SBATCH --time=72:00:00
-#SBATCH -o no_rot_sc_th_basic_net_low_z%A_%a.out
+#SBATCH -o no_rot_sc_th_basic_net_mid_z%A_%a.out
 #SBATCH --array=0-0
 # --------------------
 ### Code Section
@@ -16,9 +16,9 @@
 export OMP_NUM_THREADS=16
 
 # Array of folder names
-folders=(m1_60_m2_40_p_6_BD_no_rot_sc_th_basic_net_low_z)
+folders=(m1_60_m2_40_p_6_BD_no_rot_sc_th_basic_net_mid_z)
 
 # Navigate to the appropriate folder
 cd /groups/mrenzo/Neev/Projects/XRB/binary_star/${folders[$SLURM_ARRAY_TASK_ID]}
 ./clean && ./mk
-./re 12000
+./rn
