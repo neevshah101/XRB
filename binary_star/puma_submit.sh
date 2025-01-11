bff7031ad01f921c3e4506e64f6da60ee80fd118
Neev Shah <neevshah2001@gmail.com>
2025-01-11 02:24:04 -0700

    50+30 with alpha mlt of 3 resuming 26k

--- binary_star/puma_submit.sh
+++ binary_star/puma_submit.sh
@@ -8,7 +8,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=16
 #SBATCH --time=72:00:00
-#SBATCH -o no_rot_sc_th_basic_net_45_25_5_z_0146_inc_alpha_mlt_2.0_%A_%a.out
+#SBATCH -o no_rot_sc_th_basic_net_50_30_5_z_0146_inc_alpha_mlt_3.0_%A_%a.out
 #SBATCH --array=0-0
 # --------------------
 ### Code Section
@@ -16,7 +16,7 @@
 export OMP_NUM_THREADS=16
 
 # Array of folder names
-folders=(m1_45_m2_25_p_5_BD_no_rot_sc_th_basic_net_mid_z_inc_alpha_mlt_2.0)
+folders=(m1_50_m2_30_p_5_BD_no_rot_sc_th_basic_net_mid_z_inc_alpha_mlt_3.0)
 
 # Navigate to the appropriate folder
 cd /groups/mrenzo/Neev/Projects/XRB/binary_star/${folders[$SLURM_ARRAY_TASK_ID]}
